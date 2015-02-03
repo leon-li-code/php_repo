25030c6170474ab4a418b28b224270edd4e943f1
leon-li-code <leon_temp@163.com>
2015-02-03 11:45:30 +0800

    test

--- test/temp_test.php
+++ test/temp_test.php
@@ -1,18 +1,44 @@
 <?php
 
+
 /**
  * 测试multi_array_sort
  */
 	include ('mylib\ArrayHelper.php');
-	$tags[]=array("rank"=>1,"name"=>'zhang');
-	$tags[]=array("rank"=>2,"name"=>'sa');
-	$tags[]=array("rank"=>3,"name"=>'sx');
-	$arr=ArrayHelper::multi_array_sort($tags, 'rank');
-	print_r($arr);
+// 	$tags[]=array("rank"=>1,"name"=>'zhang');
+// 	$tags[]=array("rank"=>2,"name"=>'sa');
+// 	$tags[]=array("rank"=>3,"name"=>'sx');
+// 	$arr=ArrayHelper::multi_array_sort($tags, 'rank');
+// 	print_r($arr);
 
-	test();
-	function test()
+// 	test();
+// 	function test()
+// 	{
+// 		echo 'test';
+// 	}
+// 	$tag_value="test,as";
+// 	$tag_value_arr=explode(",",$tag_value);
+// 	var_dump($tag_value_arr);
+	
+	
+// 	$user_map=array();
+// 	$data=array(
+// 			user=>'54b37cc4db26ce6e578b6817',
+// 			user_map=>$user_map
+// 	);
+// 	print_r($data['user']);
+	
+	$y=1;
+	$x=$y==true;
+	print $x;
+	if($y==true)
 	{
-		echo 'test';
+		print "t";
 	}
-
+	else
+	{
+		print 'f';
+	}
+	
+	
+	
